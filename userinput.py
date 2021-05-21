9bb0f6853d1ab35aab249bb4188ac4e8a686826d
Angelaacholdeng <dengacholangela@gmail.com>
2021-05-21 12:52:57 +0300

    My python project 1

---
+++ userinput.py
@@ -0,0 +1,19 @@
+# greeting="Hello,welcome"
+# name=input("What is your name?")
+# print(f"{greeting} {name}")
+
+greeting="Correct,welcome to AkiraChix"
+print("What are your full names?")
+name=input()
+print("What is your nationality?")
+nationality=input()
+print("What are the akiraChix core values?")
+values=input()
+print("Enter a number")
+num1=input()
+print("Enter another number")
+num2=input()
+sum=int(num1)+int(num2)
+print(sum)
+print(f"{greeting} {name} ")
+
